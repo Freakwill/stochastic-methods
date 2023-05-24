7ad0f07d9092fec75d94c2f058b5ada30cdb6b36
William Song <30965609+Freakwill@users.noreply.github.com>
2023-05-24 21:36:39 +0800

    ?

--- codes/tca.py
+++ codes/tca.py
@@ -2,31 +2,33 @@
 
 """
 Transfer Component Analysis
+
+*ref.*
+王晋东 迁移学习导论
 """
 
 import numpy as np
 import numpy.linalg as LA
-import scipy.io
 import scipy.linalg
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.neighbors import KNeighborsClassifier
 import sklearn.metrics
 
 
-def kernel(ker,X1,X2,gamma):
-    K=None
+def kernel(ker, X1, X2, gamma):
+    K = None
     if not ker or ker == 'primal':
         K = X1
     elif ker == 'linear':
         if X2 is not None:
-            K=sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T, np.asarray(X2).T)
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T, np.asarray(X2).T)
         else:
-            K=sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T)
-    elif ker=='rbf':
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T)
+    elif ker == 'rbf':
         if X2 is not None:
-            K=sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1).T,np.asarray(X2).T,gamma)
+            K = sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1).T, np.asarray(X2).T, gamma)
         else:
-            K=sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1).T,None, gamma)
+            K = sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1).T, None, gamma)
     return K
 
 
@@ -34,7 +36,7 @@ class TCA(BaseEstimator, TransformerMixin):
     def __init__(self, kernel_type='primal', repr_ndim=30, lamb=1, gamma=1):
         '''
         Init func
-        param kernel_type:kernel,values:'prinal'|linear'l 'rbf
+        param kernel_type: kernel, values: 'primal'|linear'| 'rbf'
         :param repr_ndim: dimension after transfer
         :param lamb: lambda value in equation
         :param gamma:kernel bandwidth for rbf kernel
@@ -44,32 +46,42 @@ class TCA(BaseEstimator, TransformerMixin):
         self.lamb = lamb
         self.gamma = gamma
 
-    def fit_transform(self, Xs, Xt):
+    def fit_transform(self, Xs, Xt, normalized=False):
         '''
         Transform Xs and Xt
         :param Xs: ns * n_feature, source feature
         :param Xt: nt * n_feature, target feature
         :return:Xs_new and Xt_new after TCA
         '''
-        X = np.hstack((Xs.T,Xt.T))
+        X = np.hstack((Xs.T, Xt.T))
         X /= LA.norm(X, axis=0)
         m, n = X.shape
-        ns,nt=len(Xs),len(Xt)
+        ns, nt=len(Xs), len(Xt)
         e = np.vstack((1/ns * np.ones((ns,1)), -1/nt * np.ones((nt,1))))
-        M = e*e.T
-        M=M/LA.norm(M,'fro')
-        H=np.eye(n)-1/n*np.ones((n,n))
-        K=kernel(self.kernel_type,X,None,gamma=self.gamma)
+        M = e * e.T
+        M = M / LA.norm(M,'fro')
+        H = np.eye(n) - 1/n * np.ones((n, n))
+        K = kernel(self.kernel_type, X, None, gamma=self.gamma)
         n_eye = m if self.kernel_type == 'primal' else n
-        a, b = (LA.multi_dot([K, M, K.T]) + self.lamb*np.eye(n_eye),
+        a, b = (LA.multi_dot([K, M, K.T]) + self.lamb * np.eye(n_eye),
                LA.multi_dot([K, H, K.T]))
-        w, V = scipy.linalg.eig(a,b)
+        w, V = scipy.linalg.eig(a, b)
         ind = np.argsort(w)
         A = V[:, ind[:self.repr_ndim]]
+        self.W = A
         Z = np.dot(A.T, K)
-        Z/=LA.norm(Z,axis=0)
-        Xs_new, Xt_new=Z[:,:ns].T, Z[:,ns:].T
+        if normalized:
+            Z /= LA.norm(Z,axis=0)
+        Xs_new, Xt_new = Z[:,:ns].T, Z[:,ns:].T
         return Xs_new, Xt_new
+
+    def transform(self, X, normalized=False):
+        K = kernel(self.kernel_type, X, None, gamma=self.gamma)
+        n_eye = m if self.kernel_type == 'primal' else n
+        Z = np.dot(self.W.T, K)
+        if normalized:
+            Z /= LA.norm(Z,axis=0)
+        return X_new
 
     def fit(self):
         self.fit_transform()
@@ -92,16 +104,16 @@ class TCA(BaseEstimator, TransformerMixin):
 
 if __name__ == '__main__':
     
-    # from sklearn.datasets import load_digits
+    from sklearn.datasets import load_digits
 
-    # X, y = load_digits(return_X_y=True)
-    # Xs = X[(y<5)]
-    # Xt = X[(y>=5)]
+    X, y = load_digits(return_X_y=True)
+    Xs = X[(y<5)]
+    Xt = X[(y>=5)]
+    size = 8, 8
 
-    from datasets import load_hanzi
-    Xs_Xt = load_hanzi(channel=0, ravel=True)
-
-    Xs, Xt = Xs_Xt[:100], Xs_Xt[100:]
+    # from datasets import load_hanzi
+    # Xs_Xt = load_hanzi(channel=0, ravel=True)
+    # Xs, Xt = Xs_Xt[:100], Xs_Xt[100:]
 
     tca = TCA(repr_ndim=2)
     Xs_new, Xt_new = tca.fit_transform(Xs, Xt)
@@ -109,15 +121,15 @@ if __name__ == '__main__':
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.set_title("Demo of Transfer Component Analysis")
-    ax.set_xlim((-1.2, 1.2))
-    ax.set_ylim((-1.2, 1.2))
+    ax.set_xlim((-0.5, 0.5))
+    ax.set_ylim((-0.5, 0.5))
     import random
     for xs, xt, zs, zt in zip(Xs, Xt, Xs_new, Xt_new):
-        if random.random() < 0.1:
+        if random.random() < 0.06:
             c1, c2 = zs
-            ax.imshow(xs.reshape((40, 40)), extent=(c1-0.1, c1+0.1, c2-0.1, c2+0.1), alpha=0.5, cmap='GnBu')
+            ax.imshow(xs.reshape(size), extent=(c1-0.03, c1+0.03, c2-0.03, c2+0.03), alpha=0.5, cmap='GnBu')
             c1, c2 = zt
-            ax.imshow(xt.reshape((40, 40)), extent=(c1-0.1, c1+0.1, c2-0.1, c2+0.1), alpha=0.5, cmap='RdPu')
+            ax.imshow(xt.reshape(size), extent=(c1-0.03, c1+0.03, c2-0.03, c2+0.03), alpha=0.5, cmap='RdPu')
 
     plt.show()
 
